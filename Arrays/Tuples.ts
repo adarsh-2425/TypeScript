7f5a67da246bb08a432426d8440e46d5b1d18343
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-09-09 18:17:58 +0530

    Create Tuples.ts

---
+++ Arrays/Tuples.ts
@@ -0,0 +1,2 @@
+// In TypeScript, when an array is typed with elements of specific types, it’s called a tuple. 
+let ourTuple: [string, number, string, boolean] = ['Is', 7 , 'our favorite number?' , false]; 

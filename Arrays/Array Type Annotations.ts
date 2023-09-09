8bcc3b5b9c45b3e5f07492999d1c1e22cb5a6143
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-09-09 17:04:26 +0530

    Create Array Type Annotations.ts

---
+++ Arrays/Array Type Annotations.ts
@@ -0,0 +1,5 @@
+// Arrays:
+let bestNumbers: number[] = [7,77,4]; // only numbers can be added to this array
+let bestLunches: string[] = ['chicken soup', 'non-chicken soup'];
+let bestBreakfasts: string[]= ['scrambled eggs', 'oatmeal', 'tamago kake gohan', 'any kind of soup'];
+let bestBooleans: boolean[] = [true, false];

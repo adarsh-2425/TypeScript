442d0474c8f3a94dd5cc49dfa1132994ae10bcf2
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-09-09 13:37:01 +0530

    Create Documenting Functions.ts

---
+++ Functions/Documenting Functions.ts
@@ -0,0 +1,11 @@
+/**
+ * returns fruits for salad
+ * @param fruit1 - First fruit
+ * @param fruit2 - Second fruit
+ * @returns nothing
+ */
+
+function makeFruitSalad(fruit1: string, fruit2: string): void {
+  let salad = fruit1 + fruit2 + fruit2 + fruit1 + fruit2+ fruit1 + fruit1;
+  console.log(salad);
+}

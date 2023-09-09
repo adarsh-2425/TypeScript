4c558472dc6a62aa67b430aa902a54e5cdcbee74
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-09-09 13:40:17 +0530

    Create Inferring Return Types.ts

---
+++ Functions/Inferring Return Types.ts
@@ -0,0 +1,6 @@
+function createGreeting(name: string) {
+  return `Hello, ${name}!`;
+}
+
+// myGreeting variablw will be automatically typed to string type
+const myGreeting = createGreeting('Aisle Nevertell');
